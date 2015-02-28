da648a28079482cb4a8a95336bfde41a8451b628
Maksood Mohiuddin <maksood@gmail.com>
2015-02-27 19:49:14 -0800

    Idea of bot implementaion to play against

--- src/maksoodmohiuddin/games/tic_tac_toe/MainActivity.java
+++ src/maksoodmohiuddin/games/tic_tac_toe/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        
+        
         changeButtonStates(false);
     	startResetStates(true);
     }
